50fe0efb729ea64210c22b1c30ba51dcc92f3d70
Maulik <ganganimaulik@gmail.com>
2022-05-31 10:46:16 +0530

    init

---
+++ pages/[pid].js
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router'
+import axios from 'axios';
+
+import Image from 'next/image'
+
+
+
+function Page({ data }) {
+
+    const id = data.id
+    const title = data.title['rendered']
+    const content_in = data.content['rendered']
+
+
+
+    const content = '<!DOCTYPE html> <html> <head> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta charset="utf-8"> <meta charset="UTF-8"> <title>'+title+'</title> <meta content="en_US"> <meta content="article"> <meta content="'+title+'"> <meta content=" ..."> <meta content=""> <meta content=""> <meta content="Animal"> <meta content="https://wildlifeephotography.vercel.app/wp-content/uploads/2021/04/cover10-6-1.png"> <meta content="'+title+'"> <style> * { box-sizing: border-box; } body { font-family: Arial; padding: 20px; background: #f1f1f1; } .card { background-color: white; padding: 20px; margin-top: 20px; } @media screen and (max-width: 800px) { .leftcolumn, .rightcolumn { width: 100%; padding: 0; } } </style> </head> <body> <a href="#">Home</a> <a href="#">News</a> <a href="#">Contact</a> <div class="row"> <div class="leftcolumn"> <div class="card"> <h2>'+title+'</h2> '+content_in+' </div> </body> </html> ';
+
+
+      return (
+        <div
+          dangerouslySetInnerHTML={{__html: content}} 
+        />
+
+      );
+}
+
+
+
+export async function getServerSideProps({params}) {
+    const pid = params.pid
+    const res = await fetch(`https://lolitopia.com/?rest_route=/wp/v2/posts/${pid}`)
+    const data = await res.json()
+
+    return {
+      props: { data },
+    }
+}
+
+
+
+
+export default Page
+
