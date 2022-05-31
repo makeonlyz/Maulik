f5568e0e8271ee7c6a01958bcceab78dd7b35d66
Maulik <ganganimaulik@gmail.com>
2022-05-31 12:31:50 +0530

    update fetch method

--- pages/api/[...slug].js
+++ pages/api/[...slug].js
@@ -1,7 +1,19 @@
-var request = require("request");
+import axios from "axios";
 
 export default function handler(req, res) {
   let blog = "https://lolitopia.com/";
   let url = `${blog}${req.query.slug.join("/")}`;
-  request.get(url).pipe(res);
+
+  axios({
+    method: "get",
+    url,
+  })
+    .then(function (response) {
+      var headers = { "Content-Type": "image/jpeg" };
+      res.writeHead(200, headers);
+      res.end(response.data, "binary");
+    })
+    .catch(function (error) {
+      res.send("error:" + error);
+    });
 }
