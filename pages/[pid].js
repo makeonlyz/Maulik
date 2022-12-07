ea986bb86ccd35e27d559be35ec7b13d9ebf5d9c
makeonlyz <steamkeyshop@gmail.com>
2022-12-07 21:56:51 +0700

    Update [pid].js

--- pages/[pid].js
+++ pages/[pid].js
@@ -114,14 +114,6 @@ export async function getServerSideProps({ params, req, query }) {
         }
     }
   
-  if(redirect&&pid){
-      return {
-          redirect: {
-              permanent: false,
-              destination: `https://${config.BLOG_URL}?p=${pid}`
-          }
-      }
-  }
  
   let data;
   await dbConnect();
